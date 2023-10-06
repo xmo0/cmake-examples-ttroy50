5a98f37bd167b8fbce3573b26d25309b6bfe6ffa
xmo0 <lan_ne@163.com>
2023-10-06 19:34:34 +0800

    update

--- 01-basic/I-compiling-with-clang/pre_test.sh
+++ 01-basic/I-compiling-with-clang/pre_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ROOT_DIR=`pwd`
+echo "ROOT_DIR: $ROOT_DIR"
 dir="01-basic/I-compiling-with-clang"
 
 if [ -d "$ROOT_DIR/$dir/build.clang" ]; then
--- 01-basic/K-imported-targets/run_test.sh
+++ 01-basic/K-imported-targets/run_test.sh
@@ -2,7 +2,9 @@
 # Make sure we have the minimum cmake version
 cmake_version=`cmake --version | grep version | cut -d" " -f3`
 
+echo "test for cmake version"
 [[ "$cmake_version"  =~ ([3-9][.][5-9.][.][0-9]) ]] || exit 0
+echo "cmake version ok"
 
 echo "correct version of cmake"
 mkdir -p build && cd build && cmake .. && make
